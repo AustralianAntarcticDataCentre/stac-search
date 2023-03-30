c53f70a4813ad9c72e5134ed760464abc1b16ec7
Lewis Rockliffe <Lewis.Rockliffe@aad.gov.au>
2023-03-31 09:01:39 +1100

    fix auth

--- src/auth.mjs
+++ src/auth.mjs
@@ -1,36 +1,26 @@
-const expressJwt = require('express-jwt')
-const jwt_secret = process.env.
+import * as jwt from 'express-jwt'
+const jwt_secret = process.env.JWT_SECRET
 
-module.exports = () => {
-    function findToken (req) {
-      if (req.headers['x-access-token'] && req.headers['x-access-token'] !== '') {
-        return req.headers['x-access-token']
-      } else if (req.query && req.query.token) {
-        return req.query.token
-      } else if (req.cookies && req.cookies.aadc_token) {
-        return req.cookies.aadc_token
-      }
-  
-      return null
+function findToken (req) {
+    if (req.headers['x-access-token'] && req.headers['x-access-token'] !== '') {
+    return req.headers['x-access-token']
+    } else if (req.query && req.query.token) {
+    return req.query.token
+    } else if (req.cookies && req.cookies.aadc_token) {
+    return req.cookies.aadc_token
     }
+
+    return null
+}
   
-    const authenticate = expressJwt({
-      secret: jwt_secret,
-      credentialsRequired: true,
-      getToken: findToken,
-      algorithms: ["HS256"]
-    })
+const authenticate = () => jwt.expressjwt({
+    secret: jwt_secret,
+    credentialsRequired: true,
+    getToken: findToken,
+    algorithms: ["HS256"]
+})
   
-    const loginCheck = expressJwt({
-      secret: jwt_secret,
-      credentialsRequired: false,
-      getToken: findToken,
-      algorithms: ["HS256"]
-    })
-  
-    return {
-      findToken,
-      authenticate,
-      loginCheck
-    }
-  }
+export {
+    findToken,
+    authenticate
+}
